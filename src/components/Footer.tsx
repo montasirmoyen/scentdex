6eeb30c9c9413ad4fec04b8da3620b2b7a8f2381
montasirmoyen <montasirmoyen@gmail.com>
2026-01-28 23:16:10 -0500

    Add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="text-center text-xs lg:text-sm text-gray-500 mt-6 lg:mt-8 mb-4 px-4">
+        Images sourced from <a href="https://www.fragrantica.com/" target="_blank" rel="noopener noreferrer" className="underline hover:text-gray-700">Fragrantica</a>.
+        Data sourced from <a href="https://api.fragella.com/" target="_blank" rel="noopener noreferrer" className="underline hover:text-gray-700">Fragella API</a>.
+        All rights reserved to their respective owners.
+      </footer>
+    );
+}
