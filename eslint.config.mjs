97f29f9b8ef3c2138617e5f8b5dd2a16c8b5dbb7
montasirmoyen <montasirmoyen@gmail.com>
2026-03-11 00:15:51 -0400

    Refactor ESLint configuration to use FlatCompat for compatibility with Next.js and TypeScript

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,30 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+/* eslint-disable react/no-unescaped-entities */
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off"
+    },
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
+];
 
 export default eslintConfig;
