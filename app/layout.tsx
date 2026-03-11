b73f1f46dfce3a10995839ffb599491a94998bfb
montasirmoyen <montasirmoyen@gmail.com>
2026-03-11 01:39:48 -0400

    Add TooltipProvider and Navbar1 to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,11 @@ import { Geist, Geist_Mono, Inter } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
+import { TooltipProvider } from "@/components/ui/tooltip"
+import { Navbar1 } from "@/components/navbar1";
 import { cn } from "@/lib/utils";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'})
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
@@ -28,7 +30,12 @@ export default function RootLayout({
       className={cn("antialiased", fontMono.variable, "font-sans", inter.variable)}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        <TooltipProvider>
+          <ThemeProvider>
+              <Navbar1 />
+              {children}
+          </ThemeProvider>
+        </TooltipProvider>
       </body>
     </html>
   )
