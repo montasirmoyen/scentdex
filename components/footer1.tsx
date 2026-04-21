9193662836455e2b5ab7d47059c50cd03b1d57f1
monty <montasirmoyen@gmail.com>
2026-04-20 20:46:36 -0400

    Add dark mode support for logo image in Footer component

--- components/footer1.tsx
+++ components/footer1.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <div className='mx-auto flex max-w-7xl items-center justify-between gap-3 px-4 py-4 max-md:flex-col sm:px-6 sm:py-6 md:gap-6 md:py-8'>
         <a href='#'>
           <div className='flex items-center gap-3'>
-            <Image className='gap-3' src='/sd-logo.png' alt='Logo' width={40} height={40} />
+            <Image className='gap-3 dark:invert' src='/sd-logo.png' alt='Logo' width={40} height={40} />
             <span className='text-lg font-bold'>ScentDex</span>
           </div>
         </a>
